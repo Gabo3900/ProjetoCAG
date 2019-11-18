5d456898a31fbacd4036743f29057bbbdd2aae04
Gabriel Moura <gabo3900@outlook.com>
2019-11-18 14:07:28 -0300

    Init

--- Escola/EscolaApp/AlunoWindow.xaml.cs
+++ Escola/EscolaApp/AlunoWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace EscolaApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
