6928aae54dffbac73ae6ec47ce33ad053f27e8a8
Gabriel Moura <gabo3900@outlook.com>
2019-11-25 13:49:29 -0300

    Init

---
+++ Escola/EscolaApp/Curso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+    class Curso
+    {
+        private int id;
+        private string nome;
+        public int Id { get; }
+        public string Nome { get; }
+
+        public Curso(string n, int i)
+        {
+            nome = n;
+            id = i;
+        }
+
+        public override string ToString()
+        {
+            return nome;
+        }
+    }
+}
---
+++ Escola/EscolaApp/Turma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+    class Turma
+    {
+        private int id, cursoId;
+        public int Id {
+            get { return id; }
+        }
+        public int CursoId { get { return cursoId; } }
+        public Turma (int i, int c)
+        {
+            id = i;
+            cursoId = c;
+        }
+    }
+}
