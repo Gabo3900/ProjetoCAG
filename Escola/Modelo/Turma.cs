b245fbd84c86a12d20fcb88b504ab3caa56855ef
Gabriel Moura <gabo3900@outlook.com>
2019-11-26 08:26:08 -0300

    Init

--- Escola/Modelo/Turma.cs
+++ Escola/Modelo/Turma.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace EscolaApp
+namespace Modelo
 {
     class Turma
     {
         private int id, cursoId;
-        public int Id {
+        public int Id
+        {
             get { return id; }
         }
-        public int CursoId {
+        public int CursoId
+        {
             get { return cursoId; }
         }
-        public Turma (int i, int c)
+        public Turma(int i, int c)
         {
             id = i;
             cursoId = c;
