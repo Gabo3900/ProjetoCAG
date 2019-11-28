94dc2438e03edf15e01dc8722e7b89b3f615c377
Gabriel Moura <gabo3900@outlook.com>
2019-11-28 14:36:26 -0300

    Init

---
+++ Escola/Negocio/NTurma.cs
@@ -0,0 +1,43 @@
+﻿using Modelo;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Negocio
+{
+    class NTurma
+    {
+        private List<Turma> turmas;
+        private PTurma p;
+        public List<Turma> Listar()
+        {
+            return p.Abrir();
+        }
+        public void Inserir(Turma t)
+        {
+            turmas = p.Abrir();
+            int id = 1;
+            if (turmas.Count > 0) id = turmas.Max(x => x.Id) + 1;
+            t.Id = id;
+            turmas.Add(t);
+            p.Salvar(turmas);
+        }
+        public void Atualizar(Turma t)
+        {
+            turmas = p.Abrir();
+            Turma r = turmas.Where(x => x.Id == t.Id).Single();
+            turmas.Remove(r);
+            turmas.Add(t);
+            p.Salvar(turmas);
+        }
+        public void Excluir(Turma t)
+        {
+            turmas = p.Abrir();
+            Turma r = turmas.Where(x => x.Id == t.Id).Single();
+            turmas.Remove(r);
+            p.Salvar(turmas);
+        }
+    }
+}
